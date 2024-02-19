21749169546e7950785e648287743ec755290011
beto2125 <betobeto60@gmail.com>
2024-02-19 16:01:04 -0600

    generate ID

--- src/App.vue
+++ src/App.vue
@@ -43,11 +43,18 @@ const expense = computed( ()=> {
 //hand transaction submitted
 const handleTransactionSubmitted = (transactionData) => {
     transactions.value.push({
+        id: generateUniqueId(),
         text: transactionData.text,
         amount: transactionData.amount,
     })
 
 }
+
+//generate unique ID
+const generateUniqueId = () => {
+    return Math.floor(Math.random() * 10000000)
+}
+
 </script>
 
 <template>
