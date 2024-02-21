02031590aebdd8b3808099d87eda1201a22808f1
beto2125 <betobeto60@gmail.com>
2024-02-21 11:35:54 -0600

    addeded toastification

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@ import IncomeExpenses from './components/IncomeExpenses.vue';
 import AddTransactions from './components/AddTransactions.vue';
 import TransactionList from './components/TransactionList.vue';
 import {ref, computed, onMounted} from 'vue'
-
+import {useToast } from 'vue-toastification'
 const transactions = ref([])
+const toast = useToast()
 
 // const transactions = ref([
 //     {id: 1, text: 'Paycheck', amount: 699.99},
@@ -48,8 +49,8 @@ const handleTransactionSubmitted = (transactionData) => {
         text: transactionData.text,
         amount: transactionData.amount,
     })
-
     saveTransactionToLocalStorage()
+    toast.success('Transaction Added')
 
 }
 
@@ -62,6 +63,8 @@ const generateUniqueId = () => {
 const handleTransactionDeleted = (id) => {
     transactions.value = transactions.value.filter((transaction) => transaction.id !== id)
     saveTransactionToLocalStorage()
+    toast.success('Transaction Deleted')
+
 }
 
 //save to local storage
